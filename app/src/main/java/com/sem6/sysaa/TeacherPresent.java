d986df1d60754577ff81fae109824e44a87b01ce
himaninimje <himaninimje23@gmail.com>
2017-04-22 23:59:15 +0530

    teacher schedule ok

--- app/src/main/java/com/sem6/sysaa/TeacherPresent.java
+++ app/src/main/java/com/sem6/sysaa/TeacherPresent.java
@@ -4,20 +4,59 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Calendar;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class TeacherPresent extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class TeacherPresent extends AppCompatActivity //implements AdapterView.OnItemSelectedListener
+{
     private DatePicker datePicker;
     private Calendar calendar;
     private TextView dateView;
     private Button setDate;
     private int year, month, day;
+    private Firebase teacher;
+    private TextView tv;
+    private String today;
+    private String today_date;
+    private String subs;
+    private String sub;
+    private Spinner spinner;
+    private ListView std_list;
+    private Iterator<DataSnapshot> itr;
+    private Iterator<DataSnapshot> itr2;
+    private List<String> subjects;
+    private List<String> students;
+    private Firebase student;
+    private FirebaseAuth mAuth;
+    private Firebase student2;
+    private ArrayAdapter<String> arrayAdapter;
+    private  ArrayAdapter<String> arrayAdapter2;
+    private String subb;
+    private String uid;
+    private String tmpp;
+    private int i=0;
+    private ArrayList<String> uid_list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +78,6 @@ public class TeacherPresent extends AppCompatActivity {
         day = calendar.get(Calendar.DAY_OF_MONTH);
         showDate(year, month+1, day);
     }
-    /*public void setDate(View view) {
-        showDialog(999);
-        Toast.makeText(getApplicationContext(), "Select Date",
-                Toast.LENGTH_SHORT)
-                .show();
-    }
-*/
     @Override
     protected Dialog onCreateDialog(int id) {
         // TODO Auto-generated method stub
@@ -60,17 +92,158 @@ public class TeacherPresent extends AppCompatActivity {
             DatePickerDialog.OnDateSetListener() {
                 @Override
                 public void onDateSet(DatePicker arg0,
-                                      int arg1, int arg2, int arg3) {
+                                      int year, int month, int day) {
                     // TODO Auto-generated method stub
-                    // arg1 = year
-                    // arg2 = month
-                    // arg3 = day
-                    showDate(arg1, arg2+1, arg3);
+                    showDate(year, month+1, day);
                 }
             };
 
     private void showDate(int year, int month, int day) {
-        dateView.setText(new StringBuilder().append(day).append("/")
-                .append(month).append("/").append(year));
+        if (month<10)
+            dateView.setText(day+"-0"+month+"-"+year);
+        else
+            dateView.setText(day+"-"+month+"-"+year);
+        Date d=new Date(year,month,day);
+        int weekdaynum=d.getDay();
+        String weekDay="";
+
+        switch (weekdaynum)
+        {
+            case 0:
+                weekDay="THURSDAY";break;
+            case 1:
+                weekDay="FRIDAY";break;
+            case 2:
+                weekDay="SATURDAY";break;
+            case 3:
+                weekDay="SUNDAY";break;
+            case 4:
+                weekDay="MONDAY";break;
+            case 5:
+                weekDay="TUESDAY";break;
+            case 6:
+                weekDay="WEDNESDAY";break;
+        }
+        dateView.setText(dateView.getText()+"....\n"+weekDay);
+        //showPresent(weekDay);
     }
+    /*public  void showPresent(String day)
+    {
+        tv=(TextView)findViewById(R.id.textView5);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        students=new ArrayList<String>();
+        students.add("init");
+        std_list=(ListView) findViewById(R.id.std_list);
+
+        SimpleDateFormat sddf = new SimpleDateFormat("EEEE");
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar c = Calendar.getInstance();
+        today=sddf.format(c.getTime());
+        today_date=df.format(c.getTime());
+        subjects=new ArrayList<String>();
+        //today="MONDAY";
+        today=day;
+        mAuth=FirebaseAuth.getInstance();
+
+        teacher=new Firebase("https://sysaa-be58b.firebaseio.com/TEACHER/"+mAuth.getCurrentUser().getUid().toString().trim()
+                +"/Timetable/"+today);
+        teacher.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                itr=dataSnapshot.getChildren().iterator();
+
+                while(itr.hasNext())
+                {
+                    subs=itr.next().toString();
+                    sub=subs.substring(subs.lastIndexOf("=")+2,subs.lastIndexOf("}")-1);
+                    Log.d("sub_len",sub+" "+Integer.toString(sub.length()));
+                    subjects.add(sub);
+
+
+                }
+                Log.d("list_uid",uid_list.toString());
+
+                arrayAdapter=new ArrayAdapter<String>(TeacherPresent.this,android.R.layout.simple_spinner_item,subjects);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(arrayAdapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+    }
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        tv.setText("");
+        uid_list.clear();
+        i=0;
+        students.clear();
+
+        subb=parent.getItemAtPosition(position).toString();
+        Log.d("subject",Integer.toString(subb.length()));
+        final String attn_date="21-04-2017";
+
+        student=new Firebase("https://sysaa-be58b.firebaseio.com/Att");
+        student.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                String temp=dataSnapshot.getValue().toString();
+                Log.d("listsub",temp);
+                itr2=dataSnapshot.getChildren().iterator();
+
+                while(itr2.hasNext())
+                {
+
+                    String obj=itr2.next().toString();
+                    Log.d("whole object",obj);
+                    uid =obj.substring(obj.indexOf("=")+2,obj.indexOf(","));
+                    Log.d("student id",uid);
+                    uid_list.add(uid);
+
+                }
+                Log.d("list_uid",uid_list.toString());
+                Iterator itr3=uid_list.iterator();
+                while ((itr3.hasNext()))
+                {
+                    student2=new Firebase("https://timetable-7e2a8.firebaseio.com/Student/"+itr3.next()+"/"+attn_date);
+                    student2.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            tmpp=uid_list.get(i);
+                            i++;
+                            if(dataSnapshot.getValue().toString().contains(subb))
+                            {
+                                Toast.makeText(TeacherPresent.this, "inside studentttt", Toast.LENGTH_SHORT).show();
+
+                                students.add(tmpp);
+                                Log.d("listvw",students.toString());
+                                arrayAdapter2=new ArrayAdapter<String>(TeacherPresent.this,android.R.layout.simple_list_item_1,students);
+                                std_list.setAdapter(arrayAdapter2);
+                            }
+                        }
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }*/
 }
--- app/src/main/java/com/sem6/sysaa/TeacherSchedule.java
+++ app/src/main/java/com/sem6/sysaa/TeacherSchedule.java
@@ -1,13 +1,169 @@
 package com.sem6.sysaa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
 
 public class TeacherSchedule extends AppCompatActivity {
+    TextView sch;
+    String batch;
+    Firebase batchh,wbatch;
+    View view;
+    ListView listView,lv2;
+    Iterator<DataSnapshot> dsi;
+    FirebaseAuth mauth;
+    Bundle extras;String check;Button back;
+    private ArrayList<String> mTimeSlots = new ArrayList<>();
+    private ArrayList<String> mSubName = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_schedule);
+        sch = (TextView) findViewById(R.id.ttimetable);
+        extras = getIntent().getExtras();
+        check=null;
+        if(extras!=null)
+            check=extras.getString("day");
+        if(check!=null) {
+            sch.setText(check +" TIMETABLE");
+            back=(Button) findViewById(R.id.backpresst);
+            back.setVisibility(View.VISIBLE);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent myIntent = getIntent();
+                    String h=null;
+                    myIntent.putExtra("day",h);
+                    startActivity(myIntent);
+                }
+            });
+            listView = (ListView)findViewById(R.id.listview1t);
+            listView.setVisibility(View.VISIBLE);
+            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(TeacherSchedule.this,
+                    android.R.layout.simple_list_item_1,mTimeSlots);
+            listView.setAdapter(arrayAdapter);
+            lv2 = (ListView)findViewById(R.id.listview2t);
+            lv2.setVisibility(View.VISIBLE);
+            final ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(TeacherSchedule.this,
+                    android.R.layout.simple_list_item_1,mSubName);
+            lv2.setAdapter(arrayAdapter2);
+            mauth=FirebaseAuth.getInstance();
+            String uuid=mauth.getCurrentUser().getUid().trim();
+            String macAddress = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+            batchh = new Firebase("https://sysaa-be58b.firebaseio.com/TEACHER/" + uuid + "/Timetable/"+check);
+            batchh.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                            dsi=dataSnapshot.getChildren().iterator();
+                            while(dsi.hasNext())
+                            {
+                                String ds=dsi.next().toString();
+                                int k=ds.indexOf("key = ");
+                                String x=ds.substring(k+6,k+10);
+                                int v=ds.indexOf("value = ");
+                                int curl=ds.indexOf('}');
+                                String y=ds.substring(v+8,curl);
+                                mTimeSlots.add(x);
+                                mSubName.add(y);
+                            }
+
+
+
+                        }
+
+
+
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
+        }
+        else
+        {
+
+            sch.setText("SCHEDULE SELECT");
+            listView = (ListView) findViewById(R.id.tlistt);
+            listView.setVisibility(View.VISIBLE);
+            String[] values = new String[]{"Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Today's TimeTable"
+
+            };
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(TeacherSchedule.this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    // ListView Clicked item index
+                    int itemPosition = position;
+
+                    // ListView Clicked item value
+                    String itemValue = (String) listView.getItemAtPosition(position);
+
+                    Intent myIntent = getIntent();
+                    if (!itemValue.equals("Today's TimeTable")) {
+                        myIntent.putExtra("day", itemValue.toUpperCase());
+
+                    } else {
+                        Calendar calendar = Calendar.getInstance();
+                        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+                        switch (day) {
+                            case Calendar.SATURDAY:
+                                myIntent.putExtra("day", "Saturday".toUpperCase());
+                                break;
+                            case Calendar.MONDAY:
+                                myIntent.putExtra("day", "Monday".toUpperCase());
+                                break;
+                            case Calendar.TUESDAY:
+                                myIntent.putExtra("day", "Tuesday".toUpperCase());break;
+                            case Calendar.WEDNESDAY:
+                                myIntent.putExtra("day", "Wednesday".toUpperCase());break;
+                            case Calendar.FRIDAY:
+                                myIntent.putExtra("day", "Friday".toUpperCase());break;
+                            case Calendar.THURSDAY:
+                                myIntent.putExtra("day", "Thursday".toUpperCase());break;
+                            case Calendar.SUNDAY:
+                                Toast.makeText(TeacherSchedule.this,
+                                        "Today is Sunday, no classes" + itemValue, Toast.LENGTH_LONG)
+                                        .show();
+                        }
+                    }
+                    startActivity(myIntent);
+                }
+
+            });
+
+        }
     }
 }
