17047a6a8f74d41a15e0c07f9fe3f1002ae897e9
shubhamyelekar <2310shubham@gmail.com>
2017-04-23 18:03:54 +0530

    total and % add kiya

--- app/src/main/java/com/sem6/sysaa/Attendance.java
+++ app/src/main/java/com/sem6/sysaa/Attendance.java
@@ -9,13 +9,15 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-        import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
         import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -32,9 +34,20 @@ public class Attendance extends Fragment {
     private Calendar calendar;
     private int year, month, day;
     Bundle extras;
-    Iterator<DataSnapshot> subjctatt;
     public DatePickerDialog.OnDateSetListener myOnDateSetListener;
-    TextView att,testdate;String macAddress;
+    TextView att,testdate;
+    private Firebase tot;
+
+    private String daa;
+    private String dbms;
+    private String ai;
+    private String iwcs;
+    int pdaa=0;
+    int pdbms=0;
+    int pai=0;
+    int piwcs=0;
+    private String macAddress;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -53,21 +66,15 @@ public class Attendance extends Fragment {
                 final TextView dateatt=(TextView) view.findViewById(R.id.dateatt);
                 Firebase dateattendance= new Firebase("https://sysaa-be58b.firebaseio.com/Att/"+macAddress+"/"+check);
                 dateattendance.addValueEventListener(new ValueEventListener() {
+
+
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        subjctatt=dataSnapshot.getChildren().iterator();
-                        while(subjctatt.hasNext())
-                        {
-                            String sub=subjctatt.next().toString();
-                            int k=sub.indexOf("key = ");
-                            int v=sub.indexOf(("value = "));
-                            int curl=sub.indexOf('}');
-                            dateatt.setText(dateatt.getText()+"\n"+sub.substring(k+6,k+10)+": "+sub.substring(v+8,curl));
-                        }
-                        /*dateatt.setText("AI:\t"+dataSnapshot.child("AI").getValue(String.class));
+
+                        dateatt.setText("AI:\t"+dataSnapshot.child("AI").getValue(String.class));
                         dateatt.setText(dateatt.getText()+"\nIWCS:\t"+dataSnapshot.child("IWCS").getValue(String.class));
                         dateatt.setText(dateatt.getText()+"\nDAA:\t"+dataSnapshot.child("DAA").getValue(String.class));
-                        dateatt.setText(dateatt.getText()+"\nDBMS:\t"+dataSnapshot.child("DBMS").getValue(String.class));*/
+                        dateatt.setText(dateatt.getText()+"\nDBMS:\t"+dataSnapshot.child("DBMS").getValue(String.class));
                     }
 
                     @Override
@@ -82,15 +89,43 @@ public class Attendance extends Fragment {
         month=calendar.get(Calendar.MONTH);
         day=calendar.get(Calendar.DAY_OF_MONTH);
         att = (TextView) view.findViewById(R.id.att);
-        String macAddress= android.provider.Settings.Secure.getString(getActivity().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
-        attf=new Firebase("https://sysaa-be58b.firebaseio.com/Att/"+macAddress+"/Total");
-        attf.addValueEventListener(new com.firebase.client.ValueEventListener() {
+         macAddress= android.provider.Settings.Secure.getString(getActivity().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+// here
+        tot=new Firebase("https://sysaa-be58b.firebaseio.com/Total classes");
+        tot.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
-                att.setText("TOTAL\nAI:\t"+dataSnapshot.child("AI").getValue(String.class));
-                att.setText(att.getText()+"\nIWCS:\t"+dataSnapshot.child("IWCS").getValue(String.class));
-                att.setText(att.getText()+"\nDAA:\t"+dataSnapshot.child("DAA").getValue(String.class));
-                att.setText(att.getText()+"\nDBMS:\t"+dataSnapshot.child("DBMS").getValue(String.class));
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                daa=dataSnapshot.child("DAA").getValue().toString();
+
+                dbms=dataSnapshot.child("DBMS").getValue().toString();
+                ai=dataSnapshot.child("AI").getValue().toString();
+                iwcs=dataSnapshot.child("IWCS").getValue().toString();
+
+
+                attf=new Firebase("https://sysaa-be58b.firebaseio.com/Att/"+macAddress+"/Total");
+                attf.addValueEventListener(new com.firebase.client.ValueEventListener() {
+                    @Override
+                    public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
+                        pdaa=(Integer.parseInt(daa)>0)?Integer.parseInt(dataSnapshot.child("DAA").getValue(String.class))*100/Integer.parseInt(daa):0;
+                pdbms=(Integer.parseInt(dbms)>0)?Integer.parseInt(dataSnapshot.child("DBMS").getValue(String.class))*100/Integer.parseInt(dbms):0;
+                pai=(Integer.parseInt(ai)>0)?Integer.parseInt(dataSnapshot.child("AI").getValue(String.class))*100/Integer.parseInt(ai):0;
+                piwcs=(Integer.parseInt(iwcs)>0)?Integer.parseInt(dataSnapshot.child("IWCS").getValue(String.class))*100/Integer.parseInt(iwcs):0;
+
+
+
+                        att.setText("TOTAL\nAI:\t"+dataSnapshot.child("AI").getValue(String.class)+"/"+ai+"  "+pai +"%");
+                        att.setText(att.getText()+"\nIWCS:\t"+dataSnapshot.child("IWCS").getValue(String.class)+"/"+iwcs+"  "+piwcs +"%");
+                        att.setText(att.getText()+"\nDAA:\t"+dataSnapshot.child("DAA").getValue(String.class)+"/"+daa+"  "+pdaa +"%");
+                        att.setText(att.getText()+"\nDBMS:\t"+dataSnapshot.child("DBMS").getValue(String.class)+"/"+dbms+"  "+pdbms +"%");
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
+
+
             }
 
             @Override
@@ -98,7 +133,6 @@ public class Attendance extends Fragment {
 
             }
         });
-
         setdate=(Button) view.findViewById(R.id.setdate);
         setdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,5 +147,7 @@ public class Attendance extends Fragment {
     }
 
 }
+
+
 
 
